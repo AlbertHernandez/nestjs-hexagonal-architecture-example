dfcf96a1d6246acf446d8b582150b23775740125
Albert Hernandez <alberthernandezdev@gmail.com>
2024-06-26 20:08:01 +0200

    refactor: rename to .use-case file

--- src/contexts/payments/infrastructure/http-api/v1/create-payment/create-payment.controller.ts
+++ src/contexts/payments/infrastructure/http-api/v1/create-payment/create-payment.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post } from "@nestjs/common";
 
-import { CreatePaymentUseCase } from "@/contexts/payments/application/create-payment-use-case/create-payment-use-case";
+import { CreatePaymentUseCase } from "@/contexts/payments/application/create-payment-use-case/create-payment.use-case";
 import { PrimitivePayment } from "@/contexts/payments/domain/payment.entity";
 import { V1_PAYMENTS } from "@/contexts/payments/infrastructure/http-api/route.constants";
 import { CreatePaymentHttpDto } from "@/contexts/payments/infrastructure/http-api/v1/create-payment/create-payment.http-dto";
--- src/contexts/payments/infrastructure/payment.module.ts
+++ src/contexts/payments/infrastructure/payment.module.ts
@@ -1,6 +1,6 @@
 import { Module } from "@nestjs/common";
 
-import { CreatePaymentUseCase } from "@/contexts/payments/application/create-payment-use-case/create-payment-use-case";
+import { CreatePaymentUseCase } from "@/contexts/payments/application/create-payment-use-case/create-payment.use-case";
 import { FindPaymentByIdUseCase } from "@/contexts/payments/application/find-payment-by-id-use-case/find-payment-by-id.use-case";
 import { PaymentRepository } from "@/contexts/payments/domain/payment.repository";
 import { CreatePaymentController } from "@/contexts/payments/infrastructure/http-api/v1/create-payment/create-payment.controller";
